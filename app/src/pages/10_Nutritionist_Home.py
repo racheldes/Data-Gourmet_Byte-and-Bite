3ab27c05bb55d525597eb95affdbc92f57907e7a
mckenziewrightt <mckenziewright429@gmail.com>
2025-04-10 18:19:04 -0400

    Update 10_Nutritionist_Home.py

--- app/src/pages/10_Nutritionist_Home.py
+++ app/src/pages/10_Nutritionist_Home.py
@@ -9,49 +9,22 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-role = st.session_state.get('role', '')
-
-if role == 'Nutritionist':
-st.title(f"Welcome Nutritionist, {st.session_state['first_name']}.")
+st.title(f"Welcome USAID Worker, {st.session_state['first_name']}.")
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('Manage Recipes', 
+if st.button('Predict Value Based on Regression Model', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/11_Manage_Recipes.py')
+  st.switch_page('pages/11_Prediction_page.py')
 
-if st.button('View Ingredients from Highly Rated Recipes', 
+if st.button('Show the Simple API Demo', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/12_View_Ingredients.py')
+  st.switch_page('pages/12_API_Test.py')
 
-if st.button("Write a Review",
+if st.button("View Classification Demo",
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/13_Write_Review.py')
-
-elif role == 'USAID Worker':
-    st.title(f"Welcome USAID Worker, {st.session_state['first_name']}.")
-    st.write('')
-    st.write('')
-    st.write('### What would you like to do today?')
-
-    if st.button('Predict Value Based on Regression Model',
-                 type='primary',
-                 use_container_width=True):
-        st.switch_page('pages/11_Prediction_page.py')
-
-    if st.button('Show the Simple API Demo',
-                 type='primary',
-                 use_container_width=True):
-        st.switch_page('pages/12_API_Test.py')
-
-    if st.button("View Classification Demo",
-                 type='primary',
-                 use_container_width=True):
-        st.switch_page('pages/13_Classification.py')
-else:
-    st.title(f"Welcome, {st.session_state['first_name']}.")
-    st.warning("Unknown role. Please contact an administrator.")
+  st.switch_page('pages/13_Classification.py')
