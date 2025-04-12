8a404f50c54da494d9d07ecf287b55cbb21a9a2d
Yazmin210 <alvarado.y@northeastern.edu>
2025-04-12 14:45:44 -0400

    update data analyst

---
+++ app/src/pages/33_Data_Analyst_Home.py
@@ -0,0 +1,21 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+import requests
+
+st.set_page_config(layout = 'wide')
+
+SideBarLinks()
+
+st.title('Data Analyst Home Page')
+st.write(f"Welcome {st.session_state['first_name']}")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
+
+if st.button('View all the tags', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/31_View_All_Tags.py')
