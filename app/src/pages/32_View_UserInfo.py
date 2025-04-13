57afd0f4921fd8f6c632763bc06a396191b505d9
Yazmin210 <alvarado.y@northeastern.edu>
2025-04-12 22:38:07 -0400

    D.A updates

--- app/src/pages/32_View_UserInfo.py
+++ app/src/pages/32_View_UserInfo.py
@@ -1,20 +1,18 @@
 import logging
 logger = logging.getLogger(__name__)
+
 import streamlit as st
 import requests
 from streamlit_extras.app_logo import add_logo
 from modules.nav import SideBarLinks
 
-
 SideBarLinks()
 
 st.write("## Access all the user info reports used")
 
-
- userInfo = requests.get('http://api:4000/d/userInfo').json()
-
+userInfo = requests.get('http://api:4000/d/userInfo').json()
 
 try:
-  st.dataframe(userInfo)
+    st.dataframe(userInfo)
 except:
-  st.write("Could not connect to database to get the User Info Reports!")
+    st.write("Could not connect to database to get the User Info Reports!")
