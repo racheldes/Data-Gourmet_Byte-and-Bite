4f42fa5bc234814409214fb6902804be22b9b42c
Yazmin210 <alvarado.y@northeastern.edu>
2025-04-12 23:36:53 -0400

    create a new userInfo report

--- app/src/pages/35_Create_UserInfo.py
+++ app/src/pages/35_Create_UserInfo.py
@@ -17,7 +17,6 @@ st.write("""
 with st.form("Insert the data for your report:"):
 
     userInfo_mealPlanCount = st.number_input("Enter the amount of meal plans this user has created")
-    userInfo_lastLoggedOn = st.datetime_input("Insert the last time the user logged on")
     userInfo_commentCount = st.number_input("Enter the number of comments this user has made")
     userInfo_userID = st.number_input(
     "Enter the user's ID",
@@ -29,31 +28,13 @@ with st.form("Insert the data for your report:"):
 
     submitted = st.form_submit_button("Submit")
 
-    uploaded_file = st.file_uploader("Choose a file")
-    if uploaded_file is not None:
-         # To read file as bytes:
-        bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
-
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
-
-        # To read file as string:
-        string_data = stringio.read()
-        st.write(string_data)
-
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
-
-    if submitted: 
-        data = {}
-        data['userInfo_mealPlanCount'] = userInfo_mealPlanCount
-        data['userInfo_lastLoggedOn'] = userInfo_lastLoggedOn
-        data['userInfo_commentCount'] = userInfo_commentCount
-        data['userInfo_userID'] = userInfo_userID
-        data['recipe_directions'] = recipe_directions
-        st.write(data)
-
-        requests.post('http://api:4000/d/post-user-info', json=data)
+    if submitted:
+        userInfo_lastLoggedOn = datetime.datetime.now().isoformat()
+        user_data = {
+        "userInfo_mealPlanCount": userInfo_mealPlanCount,
+        "userInfo_lastLoggedOn": userInfo_lastLoggedOn,
+        "userInfo_commentCount": userInfo_commentCount,
+        "userInfo_userID": int(userInfo_userID)
+    }
+        
+        requests.post('http://api:4000/d/post-userInfo', json=data)
