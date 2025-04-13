be35f30642aa0baf5c9bd775e6b67a0a0f8e2ff2
Yazmin210 <alvarado.y@northeastern.edu>
2025-04-12 22:05:07 -0400

    D.A update

--- app/src/pages/34_Manage_UserInfo.py
+++ app/src/pages/34_Manage_UserInfo.py
@@ -15,4 +15,4 @@ st.write("""
 if st.button('View all user info reports here!', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/32_Manage_UserInfo.py')
+  st.switch_page('pages/32_View_UserInfo.py')
