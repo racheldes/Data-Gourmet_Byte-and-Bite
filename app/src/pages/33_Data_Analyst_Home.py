88c94560ef6f530490966e1bdc1605c3a4d05eb7
Yazmin210 <alvarado.y@northeastern.edu>
2025-04-12 21:58:10 -0400

    updating D.A user info reports

--- app/src/pages/33_Data_Analyst_Home.py
+++ app/src/pages/33_Data_Analyst_Home.py
@@ -18,3 +18,8 @@ if st.button('View all the tags used on the platform',
              type='primary',
              use_container_width=True):
   st.switch_page('pages/31_View_All_Tags.py')
+
+  if st.button('Manage User Info Reports', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/34_Manage_UserInfo.py')
---
+++ app/src/pages/34_Manage_UserInfo.py
@@ -0,0 +1,18 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+import requests
+
+#st.set_page_config(layout = 'wide')
+SideBarLinks()
+
+st.write("""
+# Manage user info reports with a variety of functions!
+""")
+
+if st.button('View all user info reports here!', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/32_Manage_UserInfo.py')
