d96ba6f50a36f02c2b5d4dda7444ea2b5359d7e5
Yazmin210 <alvarado.y@northeastern.edu>
2025-04-14 13:43:05 -0400

    update mealPlanCount route

--- app/src/pages/36_Update_MealPlanCount.py
+++ app/src/pages/36_Update_MealPlanCount.py
@@ -5,30 +5,32 @@ import streamlit as st
 from modules.nav import SideBarLinks
 import requests
 
-#st.set_page_config(layout = 'wide')
+# st.set_page_config(layout = 'wide')
 SideBarLinks()
 
 st.write("""## Update a user's meal plan count!""")
 
 with st.form("Change a Meal Plan Count"):
-  userInfoID = st.number_input("Enter the userInfoID to be updated", step=1)
-  mealPlanCount = st.number_input(
-    "Enter the Count", min_value=0, step=1, format="%d")
+    userInfoID = st.number_input("Enter the userInfoID to be updated", step=1)
+    mealPlanCount = st.number_input(
+        "Enter the Count", min_value=0, step=1, format="%d")
 
-  submitted = st.form_submit_button("Submit")
+    submitted = st.form_submit_button("Submit")
 
-    if userInfoID and mealPlanCount:
-        data = {}
-        data['userInfoID'] = userInfoID
-        data['mealPlanCount'] = mealPlanCount
+    if submitted and userInfoID and mealPlanCount:
+        data = {
+            'userInfoID': userInfoID,
+            'mealPlanCount': mealPlanCount
+        }
         st.write(data)
 
-    response = requests.put(f'http://api:4000/d/userInfo/<userinfoID>/<mealPlanCount>', json = data)
+        response = requests.put(
+            f'http://api:4000/d/userInfo/{userInfoID}/{mealPlanCount}',
+            json=data
+        )
 
-
-    if response.status_code == 200:
-      st.success("The count has been updated!")
-      st.badge("Success", icon=":material/check:", color="green")
-    else:
-      st.error("Meal plan count has failed to update! Sorry!")
-
+        if response.status_code == 200:
+            st.success("The count has been updated!")
+            st.badge("Success", icon=":material/check:", color="green")
+        else:
+            st.error("Meal plan count has failed to update! Sorry!")
